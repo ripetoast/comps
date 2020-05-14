ba9734f5846fd29923f519cacf7f037c9f99d06a
Michael <michael_lau@live.co.uk>
2020-05-14 01:40:01 +0100

    ADD: Modal

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
-
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
+  }
 }
